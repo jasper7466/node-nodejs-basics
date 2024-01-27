eeeb640fc5dc9ea5f4a35c34d0ea5365ae57a0b0
Tim <jasper7466@yandex.ru>
2024-01-27 15:52:05 +0500

    feat: implement "fs/list" function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,50 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+const __dirname = import.meta.dirname;
+
+const targetDirPath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  const isTargetDirExists = await isExists(targetDirPath);
+
+  if (!isTargetDirExists) {
+    throw new Error('FS operation failed');
+  }
+
+  try {
+    const files = await fsPromises.readdir(targetDirPath, {
+      withFileTypes: true,
+    });
+
+    for (const file of files) {
+      if (!file.isFile()) {
+        continue;
+      }
+
+      console.log(file.name);
+    }
+  } catch (error) {
+    console.error(`Something went wrong: ${error}`);
+  }
 };
 
 await list();
+
+/**
+ * Checks if specified path is exists.
+ *
+ * @param {string} path Absolute path to be checked.
+ * @returns {boolean} `true` if path exists, otherwise `false`.
+ */
+async function isExists(path) {
+  try {
+    await fsPromises.access(path);
+    return true;
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return false;
+    }
+    throw error;
+  }
+}
