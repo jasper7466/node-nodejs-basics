1dc98676f7c9b3c12a099190632fd5eb668f9716
Tim <jasper7466@yandex.ru>
2024-01-27 17:09:03 +0500

    feat: implement "cli/parseArgs" function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argv = process.argv.slice(2);
+  const parsedArgv = [];
+
+  for (let i = 0; i < argv.length - 1; i += 2) {
+    parsedArgv.push(`${argv[i].slice(2)} is ${argv[i + 1]}`);
+  }
+
+  console.log(parsedArgv.join(', '));
 };
 
 parseArgs();
